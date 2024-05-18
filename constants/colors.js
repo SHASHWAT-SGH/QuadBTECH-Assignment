ad0313f69e9e079d254693d5236998368da5fc9b
SHASHWAT-SGH <vnsshashwat@gmail.com>
2024-05-18 14:13:34 +0530

    tab bar color changed

--- constants/colors.js
+++ constants/colors.js
@@ -1,5 +1,7 @@
 const colorScheme1 = {
   color1: '#1E1D24',
+  color2: '#17161a',
+  color3: '#0c0b0d',
 };
 
 export {colorScheme1};
--- routes/TabNavigator.jsx
+++ routes/TabNavigator.jsx
@@ -4,6 +4,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import SearchScreen from '../screens/SearchScreen';
 import StackNavigatorHome from './StackNavigatorHome';
+import {colorScheme1} from '../constants/colors';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,6 +16,10 @@ const TabNavigator = () => {
         headerShown: false,
         tabBarActiveTintColor: 'tomato',
         tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          backgroundColor: colorScheme1.color2,
+          borderBlockColor: colorScheme1.color3,
+        },
       })}>
       <Tab.Screen name="homeStack" component={StackNavigatorHome} />
       <Tab.Screen name="Search" component={SearchScreen} />
